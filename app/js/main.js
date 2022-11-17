a6dd022b07a3e01bbc3c5fc7c078e2a4414fae0a
Bananius2280 <artemkharin3@gmail.com>
2022-11-17 11:58:10 +0200

    added header and burger

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,19 @@
 $(function () {
 
+  $('.header-top__right-btn').on('click', function () {
+    $('.header-top__right-btn__img').toggleClass('header-top__right-btn__img-open')
+
+    $('.header-top__right-btn__img-close').toggleClass('header-top__right-btn__img--active')
+
+    $('.header-top__right-btn__img--active').toggleClass('header-top__right-btn__img-close')
+
+    $('.header-top__right-btn__img-close').removeClass('header-top__right-btn__img--active')
+    
+  });
+
+  $('.header-top__right-btn').on('click', function () {
+    $('.header-top__burger').toggleClass('header-top__burger--active')
+  });
+
+
 });
